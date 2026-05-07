7b2295a0420e4751df10f964a9b028e4dfbf9c69
ysro <141387749+ysro@users.noreply.github.com>
2026-05-07 18:32:05 +0000

    Deploying to gh-pages from @ ysro/ysro.github.io@dddeeb70292db547032a704015ea826371d6603d 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ysro/CV",
+    "data-repo": "ysro/ysro.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
