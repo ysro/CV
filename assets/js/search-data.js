10dda19a8a7c6cb8e68e964c5c5d1e7c039fc27b
ysro <141387749+ysro@users.noreply.github.com>
2026-05-08 04:28:08 +0000

    Deploying to gh-pages from @ ysro/ysro.github.io@a3c58505828beb2257bfbc5d72f8e9bfd8edd6b8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%79%73%72%6F%33%30%36%37@%6B%61%69%73%74.%61%63.%6B%72", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -539,7 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=2I03CpgAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
